9ced9469fc7a5d2ecb552bb87b6dcb77621b61bb
JerameKim <jkidwin@gmail.com>
2020-05-28 15:24:09 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jerame Kim on 5/28/20.
+//  Copyright © 2020 Jerame Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
