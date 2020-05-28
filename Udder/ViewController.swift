468bc75b0c2ac5c60aedb6bf2c937ca7a074a4fe
JerameKim <jkidwin@gmail.com>
2020-05-28 16:06:52 -0700

    added viewDidAppear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+    }
 
 }
 
